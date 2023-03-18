c974876489c6ed23d8eabe28457cb9ff3fd82220
xxf <luzhoushili@outlook.com>
2023-03-18 09:25:09 +0800

    后端学习

--- backedn2/src/main/java/com/kob/backedn2/Backedn2Application.java
+++ backedn2/src/main/java/com/kob/backedn2/Backedn2Application.java
@@ -2,11 +2,22 @@ package com.kob.backedn2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Backedn2Application {
     public static void main(String[] args) {
         SpringApplication.run(Backedn2Application.class, args);
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        System.out.println(passwordEncoder.encode("111"));
+        System.out.println(passwordEncoder.encode("211"));
+        System.out.println(passwordEncoder.encode("21"));
+        System.out.println(passwordEncoder.encode("121"));
+
+
     }
 
 }
--- backedn2/src/main/java/com/kob/backedn2/controller/user/UserController.java
+++ backedn2/src/main/java/com/kob/backedn2/controller/user/UserController.java
@@ -1,8 +1,11 @@
 package com.kob.backedn2.controller.user;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kob.backedn2.mapper.UserMapper;
 import com.kob.backedn2.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,33 +14,37 @@ import java.util.List;
 
 @RestController
 public class UserController {
+
     @Autowired
     UserMapper userMapper;
-//     注解开发  获取所有的用户
+
     @GetMapping("/user/all/")
-    public List<User> getAll(){
+    public List<User> getAll() {
         return userMapper.selectList(null);
     }
-//    查询指定id的信息  userid 使用{} 进行包围
+
     @GetMapping("/user/{userId}/")
-    public User getuser(@PathVariable int userId){
+    public User getuser(@PathVariable int userId) {
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("id",userId);
+        queryWrapper.eq("id", userId);
         return userMapper.selectOne(queryWrapper);
     }
 
-    // 插入指定用户数据
     @GetMapping("/user/add/{userId}/{username}/{password}/")
-    public String addUser(@PathVariable int userId,@PathVariable String username,@PathVariable String password){
-        User user = new User(userId,username,password);
+    public String addUser(
+            @PathVariable int userId,
+            @PathVariable String username,
+            @PathVariable String password) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = new User(userId, username, encodedPassword);
         userMapper.insert(user);
         return "Add User Successfully";
     }
 
-    // 删除用户   判断urL格式  然后调用该函数
     @GetMapping("/user/delete/{userId}/")
-    public String deleteUser(@PathVariable int userId){
-        userMapper.deleteById(userId);// 根据Id删除用户
+    public String deleteUser(@PathVariable int userId) {
+        userMapper.deleteById(userId);
         return "Delete User Successfully";
     }
 }
