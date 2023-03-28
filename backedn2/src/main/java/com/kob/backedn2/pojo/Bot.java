88f2b4bcfcd8dfcce0c986a51ce82e907b2bbc8f
xxf <luzhoushili@outlook.com>
2023-03-28 23:56:47 +0800

    个人中心页面提交

--- backedn2/src/main/java/com/kob/backedn2/pojo/Bot.java
+++ backedn2/src/main/java/com/kob/backedn2/pojo/Bot.java
@@ -23,8 +23,8 @@ public class Bot {
     private Integer rating;
 
     // 注解添加日期格式
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone  = "Asia/Shanghai")
     private Date createtime;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone  = "Asia/Shanghai")
     private Date modifytime;
 }
